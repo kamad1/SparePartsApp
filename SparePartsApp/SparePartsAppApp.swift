94d04e2bc5e858497d0868158f49e7a57fc23aef
KaMaDi <artkamadi@gmail.com>
2024-05-26 16:55:50 +0300

    Initial Commit

---
+++ SparePartsApp/SparePartsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparePartsAppApp.swift
+//  SparePartsApp
+//
+//  Created by Jedi on 26.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SparePartsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
